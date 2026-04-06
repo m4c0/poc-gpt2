2f2f85e5586c4bf832784d98d0efd2de12889f18
Eduardo Costa <m4c0@users.noreply.github.com>
2026-04-06 16:04:31 +0100

    vocab: proper decoding

--- vocab.c
+++ vocab.c
@@ -53,11 +53,17 @@ static char * utl_slurp(const char * file) {
 /// Maps bytes of UTF-8 into multibyte chars used in vocab.bpe.
 /// Oddly enough, multibyte UTF-8 will be mapped as multiple wchars.
 static wchar_t byt_map[256] = {0};
+static char byt_rev_map[65536] = {0};
 
 static wchar_t * byt_encode_bytes(const char * b, unsigned bytes) {
   wchar_t * mb = malloc(sizeof(wchar_t) * bytes);
   for (int i = 0; i < bytes; i++) mb[i] = byt_map[(unsigned)b[i]];
   return mb;
+}
+static int byt_decode_bytes(utl_wstr_t str, char * dst, unsigned dsz) {
+  int i;
+  for (i = 0; i < str.sz && i < dsz; i++) dst[i] = byt_rev_map[str.str[i]];
+  return i;
 }
 static void byt_init() {
   for (unsigned c = '!'; c <= '~'; c++) byt_map[c] = c;
@@ -66,6 +72,8 @@ static void byt_init() {
 
   wchar_t mc = 256;
   for (unsigned c = 0; c <= 255; c++) if (!byt_map[c]) byt_map[c] = mc++;
+
+  for (int i = 0; i < 256; i++) byt_rev_map[byt_map[i]] = i;
 
   assert(byt_map[0] == 256);
   assert(byt_map[33] == 33);
@@ -344,7 +352,7 @@ int main() {
   char * c = buf;
   for (int i = 0; i < ts.sz; i++) {
     utl_wstr_t tk = enc_map[ts.ids[i]];
-    int n = wcstombs(c, utl_wstr_printable(tk), len);
+    int n = byt_decode_bytes(tk, c, len);
     c += n;
     len -= n;
   }
