299119b4897b6a45a6ae17ef64df62b0053511bc
Eduardo Costa <m4c0@users.noreply.github.com>
2026-04-13 23:22:31 +0100

    gpt2: all transformers

--- gpt2.c
+++ gpt2.c
@@ -771,6 +771,11 @@ static void load_tensor(vlk_buffer_t b, const char * name, unsigned s0, unsigned
   sft_get(name, ptr, s0, s1, s2, s3);
   vkUnmapMemory(vlk_dev, b.mem);
 }
+static void load_tr_tensor(vlk_buffer_t b, int idx, const char * name, unsigned s0, unsigned s1, unsigned s2, unsigned s3) {
+  char buf[256];
+  sprintf(buf, "h.%d.%s", idx, name);
+  load_tensor(b, buf, s0, s1, s2, s3);
+}
 
 static VkCommandBuffer alloc() {
   VkCommandBuffer cb = vlk_allocate_command_buffer();
@@ -855,89 +860,90 @@ int main() {
   submit(cb);
 
   //--- Transform
- 
-  load_tensor(b_ln1w, "h.0.ln_1.weight", 768, 0, 0, 0);
-  load_tensor(b_ln1b, "h.0.ln_1.bias",   768, 0, 0, 0);
-  load_tensor(b_ln2w, "h.0.ln_2.weight", 768, 0, 0, 0);
-  load_tensor(b_ln2b, "h.0.ln_2.bias",   768, 0, 0, 0);
-  load_tensor(b_cattn_w, "h.0.attn.c_attn.weight",  768, 2304, 0, 0);
-  load_tensor(b_cattn_b, "h.0.attn.c_attn.bias",   2304,    0, 0, 0);
-  load_tensor(b_cproj_w, "h.0.attn.c_proj.weight",  768,  768, 0, 0);
-  load_tensor(b_cproj_b, "h.0.attn.c_proj.bias",    768,    0, 0, 0);
-  load_tensor(b_mlpcf_w, "h.0.mlp.c_fc.weight",     768, 3072, 0, 0);
-  load_tensor(b_mlpcf_b, "h.0.mlp.c_fc.bias",      3072,    0, 0, 0);
-  load_tensor(b_mlpcp_w, "h.0.mlp.c_proj.weight",  3072,  768, 0, 0);
-  load_tensor(b_mlpcp_b, "h.0.mlp.c_proj.bias",     768,    0, 0, 0);
+  for (int i = 0; i < 12; i++) {
+    load_tr_tensor(b_ln1w,    i, "ln_1.weight",         768,    0, 0, 0);
+    load_tr_tensor(b_ln1b,    i, "ln_1.bias",           768,    0, 0, 0);
+    load_tr_tensor(b_ln2w,    i, "ln_2.weight",         768,    0, 0, 0);
+    load_tr_tensor(b_ln2b,    i, "ln_2.bias",           768,    0, 0, 0);
+    load_tr_tensor(b_cattn_w, i, "attn.c_attn.weight",  768, 2304, 0, 0);
+    load_tr_tensor(b_cattn_b, i, "attn.c_attn.bias",   2304,    0, 0, 0);
+    load_tr_tensor(b_cproj_w, i, "attn.c_proj.weight",  768,  768, 0, 0);
+    load_tr_tensor(b_cproj_b, i, "attn.c_proj.bias",    768,    0, 0, 0);
+    load_tr_tensor(b_mlpcf_w, i, "mlp.c_fc.weight",     768, 3072, 0, 0);
+    load_tr_tensor(b_mlpcf_b, i, "mlp.c_fc.bias",      3072,    0, 0, 0);
+    load_tr_tensor(b_mlpcp_w, i, "mlp.c_proj.weight",  3072,  768, 0, 0);
+    load_tr_tensor(b_mlpcp_b, i, "mlp.c_proj.bias",     768,    0, 0, 0);
 
-  cb = alloc();
+    cb = alloc();
 
-  // Normalisation
+    // Normalisation
 
-  bind(cb, p_plsum, 2, b_x0, b_lmean);
-  vkCmdDispatch(cb, 1024, 1, 1);
-  bind(cb, p_lvari, 3, b_x0, b_lmean, b_xtmp);
-  vkCmdDispatch(cb, 1024, 768, 1);
-  bind(cb, p_plsum, 2, b_xtmp, b_lvari);
-  vkCmdDispatch(cb, 1024, 1, 1);
-  bind(cb, p_lnorm, 6, b_ln1w, b_ln1b, b_lmean, b_lvari, b_x0, b_x1);
-  vkCmdDispatch(cb, 1024, 768, 1);
-
-  // Multi-head attention - linear
-
-  unsigned k = 768;
-  vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
-  bind(cb, p_cattn, 4, b_cattn_w, b_cattn_b, b_x1, b_qkv);
-  vkCmdDispatch(cb, 1024, 2304, 1);
-
-  for (unsigned i = 0; i < 12; i++) {
-    // b_qkv contains all data for Q, followed by K, followed by V Then each of
-    // QKV is split into heads (12). Or: split 2304 into 3, then each 768 into
-    // 12 to be 64 per head
-    vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &i);
-    bind(cb, p_atscr, 2, b_qkv, b_h);
-    vkCmdDispatch(cb, 1024, 1024, 1);
-    bind(cb, p_psmax, 2, b_h, b_h);
+    bind(cb, p_plsum, 2, b_x0, b_lmean);
     vkCmdDispatch(cb, 1024, 1, 1);
-    vkCmdPushConstants(cb, p_smaxv.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &i);
-    bind(cb, p_smaxv, 3, b_h, b_qkv, b_xtmp);
-    vkCmdDispatch(cb, 1024, 64, 1);
+    bind(cb, p_lvari, 3, b_x0, b_lmean, b_xtmp);
+    vkCmdDispatch(cb, 1024, 768, 1);
+    bind(cb, p_plsum, 2, b_xtmp, b_lvari);
+    vkCmdDispatch(cb, 1024, 1, 1);
+    bind(cb, p_lnorm, 6, b_ln1w, b_ln1b, b_lmean, b_lvari, b_x0, b_x1);
+    vkCmdDispatch(cb, 1024, 768, 1);
+
+    // Multi-head attention - linear
+
+    unsigned k = 768;
+    vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
+    bind(cb, p_cattn, 4, b_cattn_w, b_cattn_b, b_x1, b_qkv);
+    vkCmdDispatch(cb, 1024, 2304, 1);
+
+    for (unsigned i = 0; i < 12; i++) {
+      // b_qkv contains all data for Q, followed by K, followed by V Then each of
+      // QKV is split into heads (12). Or: split 2304 into 3, then each 768 into
+      // 12 to be 64 per head
+      vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &i);
+      bind(cb, p_atscr, 2, b_qkv, b_h);
+      vkCmdDispatch(cb, 1024, 1024, 1);
+      bind(cb, p_psmax, 2, b_h, b_h);
+      vkCmdDispatch(cb, 1024, 1, 1);
+      vkCmdPushConstants(cb, p_smaxv.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &i);
+      bind(cb, p_smaxv, 3, b_h, b_qkv, b_xtmp);
+      vkCmdDispatch(cb, 1024, 64, 1);
+    }
+
+    vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
+    bind(cb, p_cattn, 4, b_cproj_w, b_cproj_b, b_xtmp, b_x1);
+    vkCmdDispatch(cb, 1024, 768, 1);
+
+    // Add residue
+    bind(cb, p_add2b, 2, b_x1, b_x0);
+    vkCmdDispatch(cb, 1024 * 768, 1, 1);
+
+    // Normalization
+
+    bind(cb, p_plsum, 2, b_x0, b_lmean);
+    vkCmdDispatch(cb, 1024, 1, 1);
+    bind(cb, p_lvari, 3, b_x0, b_lmean, b_xtmp);
+    vkCmdDispatch(cb, 1024, 768, 1);
+    bind(cb, p_plsum, 2, b_xtmp, b_lvari);
+    vkCmdDispatch(cb, 1024, 1, 1);
+    bind(cb, p_lnorm, 6, b_ln2w, b_ln2b, b_lmean, b_lvari, b_x0, b_x1);
+    vkCmdDispatch(cb, 1024, 768, 1);
+
+    // Multi-layer perceptron
+
+    bind(cb, p_cattn, 4, b_mlpcf_w, b_mlpcf_b, b_x1, b_mlp);
+    vkCmdDispatch(cb, 1024, 3072, 1);
+    bind(cb, p_pgelu, 1, b_mlp);
+    vkCmdDispatch(cb, 1024 * 3072, 1, 1);
+    k = 3072;
+    vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
+    bind(cb, p_cattn, 4, b_mlpcp_w, b_mlpcp_b, b_mlp, b_x1);
+    vkCmdDispatch(cb, 1024, 768, 1);
+
+    // Add residue
+    bind(cb, p_add2b, 2, b_x1, b_x0);
+    vkCmdDispatch(cb, 1024 * 768, 1, 1);
+
+    submit(cb);
   }
-
-  vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
-  bind(cb, p_cattn, 4, b_cproj_w, b_cproj_b, b_xtmp, b_x1);
-  vkCmdDispatch(cb, 1024, 768, 1);
-
-  // Add residue
-  bind(cb, p_add2b, 2, b_x1, b_x0);
-  vkCmdDispatch(cb, 1024 * 768, 1, 1);
-
-  // Normalization
-
-  bind(cb, p_plsum, 2, b_x0, b_lmean);
-  vkCmdDispatch(cb, 1024, 1, 1);
-  bind(cb, p_lvari, 3, b_x0, b_lmean, b_xtmp);
-  vkCmdDispatch(cb, 1024, 768, 1);
-  bind(cb, p_plsum, 2, b_xtmp, b_lvari);
-  vkCmdDispatch(cb, 1024, 1, 1);
-  bind(cb, p_lnorm, 6, b_ln2w, b_ln2b, b_lmean, b_lvari, b_x0, b_x1);
-  vkCmdDispatch(cb, 1024, 768, 1);
-
-  // Multi-layer perceptron
-
-  bind(cb, p_cattn, 4, b_mlpcf_w, b_mlpcf_b, b_x1, b_mlp);
-  vkCmdDispatch(cb, 1024, 3072, 1);
-  bind(cb, p_pgelu, 1, b_mlp);
-  vkCmdDispatch(cb, 1024 * 3072, 1, 1);
-  k = 3072;
-  vkCmdPushConstants(cb, p_atscr.pl, VK_SHADER_STAGE_COMPUTE_BIT, 0, 4, &k);
-  bind(cb, p_cattn, 4, b_mlpcp_w, b_mlpcp_b, b_mlp, b_x1);
-  vkCmdDispatch(cb, 1024, 768, 1);
-
-  // Add residue
-  bind(cb, p_add2b, 2, b_x1, b_x0);
-  vkCmdDispatch(cb, 1024 * 768, 1, 1);
-
-  submit(cb);
 
   float * x;
   VkDeviceMemory mem = b_x0.mem;
